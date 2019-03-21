96df1fa452ce17f55638fc2879465955392db9bc
Greg Morgan <gregm9999@gmail.com>
2019-03-21 01:26:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let recipes ={};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return  Object.assign({}, object, [key]: value)
+  return  Object.assign({}, object, {[key]: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
